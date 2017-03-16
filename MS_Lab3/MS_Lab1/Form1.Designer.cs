5e10cd20eec1c31e91e29e9fc8ed87687f24a78a
DESKTOP-FT778CM\corov <corovan1234578@gmail.com>
2017-03-16 23:15:07 +0200

    ssefsdf

---
+++ MS_Lab3/MS_Lab1/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿using Tao.Platform.Windows;
+
+namespace MS_Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.AnT = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AnT
+            // 
+            this.AnT.AccumBits = ((byte)(0));
+            this.AnT.AutoCheckErrors = false;
+            this.AnT.AutoFinish = false;
+            this.AnT.AutoMakeCurrent = true;
+            this.AnT.AutoSwapBuffers = true;
+            this.AnT.BackColor = System.Drawing.Color.Black;
+            this.AnT.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("AnT.BackgroundImage")));
+            this.AnT.ColorBits = ((byte)(32));
+            this.AnT.DepthBits = ((byte)(16));
+            this.AnT.Location = new System.Drawing.Point(12, 12);
+            this.AnT.Name = "AnT";
+            this.AnT.Size = new System.Drawing.Size(522, 299);
+            this.AnT.StencilBits = ((byte)(0));
+            this.AnT.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(428, 79);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(99, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Visible = false;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(428, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Графік у=ах^2";
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(431, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(96, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "2";
+            this.textBox2.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(164, 365);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(62, 336);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Змінити колір";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(418, 127);
+            this.trackBar2.Minimum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(109, 45);
+            this.trackBar2.TabIndex = 5;
+            this.trackBar2.Value = 1;
+            this.trackBar2.Visible = false;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(287, 354);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Колір 2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(164, 335);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 24);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Стоп";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(546, 422);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.AnT);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Лаба3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private SimpleOpenGlControl AnT;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ MS_Lab3/MS_Lab1/Form1.cs
@@ -0,0 +1,446 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tao.FreeGlut;
+using Tao.OpenGl;
+
+namespace MS_Lab1
+{
+    public partial class Form1 : Form
+    {
+        //int a = Int32.Parse(textBox2.Text);
+        public Form1()
+        {
+            InitializeComponent();
+            AnT.InitializeContexts();
+         
+        }
+        // розміри вікна 
+        double ScreenW, ScreenH;
+        
+        
+        int a ;
+        int functionRotationAngle = 0;
+        int functionRotationLines=0;
+        int FunctionRotationSpeed = 1;
+        int FunctionRotationLines = 1;
+        Color color1;
+        Color color2;
+
+        private float devX;
+        private float devY;
+
+        // зберігаєм x y точки графіку
+        private float[,] GrapValuesArray;
+        // кількість елементів  в масиві
+        private int elements_count = 0;
+
+        // прапорець, що показує заповненість масиву
+        private bool not_calculate = true;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            a = Int32.Parse(textBox2.Text);
+            // виклик бібліотеки
+            Glut.glutInit();
+            // ініціалізація режиму екрана
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE);
+
+            // встановлення кольору очистки екрана 
+            Gl.glClearColor(255, 255, 255, 1);
+
+            // встановлення порту виводу
+            Gl.glViewport(0, 0, AnT.Width, AnT.Height);
+
+            // активація проекційної матриціі
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            // очистка матриці 
+            Gl.glLoadIdentity();
+
+            // визначення параметрів налаштувань  проекції в залежності від розмірів сторін елемента AnT. 
+            if ((float)AnT.Width <= (float)AnT.Height)
+            {
+                ScreenW = 30.0;
+                ScreenH = 30.0 * (float)AnT.Height / (float)AnT.Width;
+                Glu.gluOrtho2D(0.0, ScreenW, 0.0, ScreenH);
+            }
+            else
+            {
+                ScreenW = 30.0 * (float)AnT.Width / (float)AnT.Height;
+                ScreenH = 30.0;
+                Glu.gluOrtho2D(0.0, 15.0 * (float)AnT.Width / (float)AnT.Height, 0.0, 30.0);
+            }
+
+            // збереження коефіціентів, які нам необхідні для переведення вказівника в віконній системі в координати, 
+            // прийняті в OpenGL сцені 
+           devX = (float)ScreenW / (float)AnT.Width;
+           devY = (float)ScreenH / (float)AnT.Height;
+          
+            // установка обєктно-видової матриці
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+
+           // timer1.Start();
+
+        }
+
+       
+     
+
+        private void PrintText2D(float x, float y, string text)
+        {
+
+            // встановлюємо позицію виводу 
+            Gl.glRasterPos2f(x, y);
+
+           //перебираєм значення text
+            foreach (char char_for_draw in text)
+            {
+                // символ C візуалізуємо з допомогою функції glutBitmapCharacter, викор шрифт GLUT_BITMAP_9_BY_15. 
+                Glut.glutBitmapCharacter(Glut.GLUT_BITMAP_9_BY_15, char_for_draw);
+            }
+
+        }
+
+          
+        private void functionCalculation()
+        {
+
+            // визначення локальних змінних X и Y 
+            float x = 0, y = 0;
+
+            // масив з якого будуватиметься графік
+
+            GrapValuesArray = new float[600, 2];
+
+            // лічильник елементів  масиву
+            elements_count = 0;
+
+            // вирахування всіх y для x, що належать проміжку від  -15 до 15 з кроком 0.01f 
+            for (x = -15; x < 15; x += 0.1f)
+            {
+                // вирахунок х для у
+                // по формулі y = (float)Math.Sin(x)*10; 
+                // формула. 
+                y = a*x*x;
+
+                // запис коорд x 
+                GrapValuesArray[elements_count, 0] = x;
+                // запис коорд y 
+                GrapValuesArray[elements_count, 1] = y;
+                // підрахунок елементів 
+                elements_count++;
+
+            }
+            for (x = -15; x < 15; x += 0.1f)
+            {
+                y = x;
+
+                GrapValuesArray[elements_count, 0] = 0;
+                // запис коорд y 
+                GrapValuesArray[elements_count, 1] = y;
+                elements_count++;
+            }
+                // прапорець вирахування координат 
+                not_calculate = false;
+
+        }
+
+       
+
+       
+
+        
+
+        private void Draw()
+        {
+
+          
+            //очистка буфера кольору і буфера глибини
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+
+          
+            //очищення текстури маьтриці
+            Gl.glLoadIdentity();
+
+         //установка чорного кольору
+            Gl.glColor3f(0, 0, 0);
+
+            //поміщаємо стан матриці в стек матриці
+            Gl.glPushMatrix();
+
+            // виконуємо переміщення по х і у
+            Gl.glTranslated(15, 15, 0);
+
+            // активація реживу рисування(Вказані точки будуть виводитись як точки GL_POINTS) 
+            Gl.glBegin(Gl.GL_POINTS);
+
+         //створюєм сітку із точок
+            for (int ax = -15; ax < 15; ax++)
+            {
+                for (int bx = -15; bx < 15; bx++)
+                {
+                   
+                   
+                    Gl.glVertex2d(ax, bx);
+                 
+                }
+            }
+
+          
+
+            // завершуємо малювати примітиви
+            Gl.glEnd();
+            //Gl.glPushMatrix();
+            Gl.glRotated(functionRotationLines, 0, 0, 1);
+            // режим малювання, обєднуєм в лінію кожні 2 точки
+           
+            Gl.glBegin(Gl.GL_LINES);
+
+            // коорд осі
+            Gl.glVertex2d(0, -15);
+            Gl.glVertex2d(0, 15);
+
+            Gl.glVertex2d(-15, 0);
+            Gl.glVertex2d(15, 0);
+
+            // вертикальна стрілка 
+            Gl.glVertex2d(0, 15);
+            Gl.glVertex2d(0.1, 14.5);
+            Gl.glVertex2d(0, 15);
+            Gl.glVertex2d(-0.1, 14.5);
+
+            // горизонтальна стрілка 
+            Gl.glVertex2d(15, 0);
+            Gl.glVertex2d(14.5, 0.1);
+            Gl.glVertex2d(15, 0);
+            Gl.glVertex2d(14.5, -0.1);
+
+            // завершуємом режим малювання
+            Gl.glEnd();
+            //Gl.glPopMatrix();
+            // виводимо написи на графіках 
+            PrintText2D(15.5f, 0, "x");
+            PrintText2D(0.5f, 14.5f, "y");
+
+            // викликамо функцію побудови графіка
+            Gl.glLineWidth(1);
+         
+
+            //Х
+            for (int ax = -24; ax <= 20; ax += 2)
+            {
+                Gl.glBegin(Gl.GL_LINES);
+                Gl.glColor3f(0, 0, 0);
+                Gl.glVertex2f(-0.5f, ax);
+                Gl.glVertex2f(0.5f, ax);
+
+                Gl.glEnd();
+                PrintText2D(ax, -1.5f, ax.ToString());
+            }
+
+            //У
+            for (int ax = 20; ax >= -24; ax -= 2)
+            {
+                Gl.glBegin(Gl.GL_LINES);
+                Gl.glColor3f(0, 0, 0);
+                Gl.glVertex2f(ax, -0.5f);
+                Gl.glVertex2f(ax, 0.5f);
+                Gl.glEnd();
+                PrintText2D(-1.5f, ax, ax.ToString());
+            }
+
+                // виводимо написи на графіках 
+           // PrintText2D(-6.7f, 1.0f, "-PI");
+            //PrintText2D(11.5f, 1.0f, "2PI");
+
+        
+
+            // вертаєм матрицю з стека
+            Gl.glPopMatrix();
+
+            // очікуємо завершення візуалізації кадра
+            Gl.glFlush();
+
+            // сигнал для оновлення елемента 
+            AnT.Invalidate();
+
+        }
+
+       
+
+        public void DrawNet()
+        {
+
+
+            Gl.glLineWidth(1);
+           
+
+            //Х
+            for (int ax = -16; ax <= 14; ax+=2)
+            {
+                Gl.glBegin(Gl.GL_LINES);
+                Gl.glColor3f(0, 0, 0);
+                Gl.glVertex2f(-0.5f, ax);
+                Gl.glVertex2f(0.5f, ax);
+
+                Gl.glEnd();
+                PrintText2D(ax, -1.5f, ax.ToString());
+            }
+
+            //У
+            for (int ax = 16; ax >= -14; ax-=2)
+            {
+                Gl.glBegin(Gl.GL_LINES);
+                Gl.glColor3f(0, 0, 0);
+                Gl.glVertex2f(ax, -0.5f);
+                Gl.glVertex2f(ax, 0.5f);
+                Gl.glEnd();
+                PrintText2D(-1.5f, ax, ax.ToString());
+            }
+           
+            // завершуємо малювати примітиви
+
+
+            // режим малювання, обєднуєм в лінію кожні 2 точки
+       
+         
+           
+            // завершуємом режим малювання
+           
+
+            // виводимо написи на графіках 
+           // PrintText2D(-6.7f, 1.0f, "-PI");
+            //PrintText2D(11.5f, 1.0f, "2PI");
+
+        
+         
+
+            // очікуємо завершення візуалізації кадра
+            Gl.glFlush();
+
+            // сигнал для оновлення елемента 
+            AnT.Invalidate();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            functionRotationAngle+=FunctionRotationSpeed;
+            //functionRotationLines += FunctionRotationLines;
+            functionCalculation();
+
+            Draw();
+            DrawDiagram();
+            
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            FunctionRotationSpeed = trackBar1.Value;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            FunctionRotationLines= trackBar2.Value;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            button3.BackColor = colorDialog1.Color;
+            color1 = colorDialog1.Color;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            button4.BackColor = colorDialog1.Color;
+            color2 = colorDialog1.Color;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DrawDiagram()
+        {
+
+            // перевіряємо чи координати вже вирахувані
+            if (not_calculate)
+            {//якщо ні..рахуємо 
+                functionCalculation();
+            }
+
+            // візуалізація точок -режим
+            // точки в лінії(GL_LINE_STRIP) 
+             Gl.glLineWidth(5);
+
+
+            Gl.glPushMatrix();
+            Gl.glTranslated(15,15,0);
+            Gl.glRotated(functionRotationAngle, 0, 0, 1);
+            Gl.glBegin(Gl.GL_LINE_STRIP);
+
+            // рисуємо початкову точку
+            Gl.glVertex2d(GrapValuesArray[0, 0], GrapValuesArray[0, 1]);
+
+
+            // проходимо по масиву точок
+
+            for (int ax = 1; ax < elements_count-300; ax += 2)
+            {
+
+
+                if (GrapValuesArray[ax, 1] < 0) Gl.glColor3f(color1.R/255.0f, color1.G/ax, color1.B/127);
+                else
+                 if (GrapValuesArray[ax, 1] > 0) Gl.glColor3f(color1.R/ax, color1.G / 255.0f, color1.B/127);
+
+                Gl.glVertex2d(GrapValuesArray[ax, 0], GrapValuesArray[ax, 1]);
+
+
+            }
+            // завершуємо режим малювання
+            Gl.glEnd();
+            Gl.glPopMatrix();
+            Gl.glLineWidth(5);
+
+
+            Gl.glPushMatrix();
+            Gl.glTranslated(15, 15, 0);
+            Gl.glRotated(functionRotationAngle, 0, 0, 1);
+            Gl.glBegin(Gl.GL_LINE_STRIP);
+            for (int ax = 300; ax < elements_count; ax += 2)
+            {
+
+
+                if (GrapValuesArray[ax, 1] < 0) Gl.glColor3f(color1.R / 255.0f, color1.G / ax, color1.B / 127);
+                else
+                 if (GrapValuesArray[ax, 1] > 0) Gl.glColor3f(color1.R / ax, color1.G / 255.0f, color1.B / 127);
+
+                Gl.glVertex2d(GrapValuesArray[ax, 0], GrapValuesArray[ax, 1]);
+
+
+            }
+            Gl.glEnd();
+            Gl.glPopMatrix();
+        }
+
+    }
+}
